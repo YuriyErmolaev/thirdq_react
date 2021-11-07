d34f3fae937af78794c3821e59879b68f4aa816d
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2021-11-07 13:36:24 +0700

    add message component with prop

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {Message} from "./components/Message/Message";
+
+const sendMessage = 'Hello world! )';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Message message={sendMessage} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
