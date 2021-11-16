0cadf53b6a1bfeafd31e81412625e718f9574016
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2021-11-16 10:27:45 +0700

    add material ui elements and focus

--- myreactapp/src/components/MessageForm/MessageForm.js
+++ myreactapp/src/components/MessageForm/MessageForm.js
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import {Input, TextField} from '@mui/material';
+import { Button } from '@mui/material';
+import { LoadingButton } from '@mui/lab'
 
 export const MessageForm = ({ addMessageTolist }) => {
+    const tfRef = useRef();
     const [value, setValue] = useState('');
     const handleChange = (e) => {
         setValue(e.target.value);
@@ -12,10 +16,28 @@ export const MessageForm = ({ addMessageTolist }) => {
             author: 'Yuriy'
         });
     }
+
+    useEffect(() => {
+        const tf = tfRef.current,
+            input = tf.getElementsByTagName('input')[0];
+        input.focus();
+    }, [tfRef.current]);
+
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={value} onChange={handleChange} name='message' />
-            <button className={'button-84'} type={"submit"}>Add message</button>
+            <TextField
+                ref={tfRef}
+                value={value}
+                onChange={handleChange}
+                name='message'
+            />
+            <LoadingButton
+                className={'button-84'}
+                type={"submit"}
+            >
+                Add message
+            </LoadingButton>
+
         </form>
     )
 }
